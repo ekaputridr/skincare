e67f355bb02e9c238de42c850cb9cc2e960e7f09
ekaputridr <60589563+ekaputridr@users.noreply.github.com>
2020-04-16 12:56:32 +0700

    Add files via upload

---
+++ product/home.php
@@ -0,0 +1,226 @@
+<?php
+session_start();
+$connect = mysqli_connect("localhost", "root", "", "skincare");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Macflari Product</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+<body>
+<br>
+<div class="page-header">
+  <a href="act.php"><center><h10>FREE SHIPPING OVER RP.100,000 FOR MEMBERSHIP</h10></center></a>
+  <ul>
+    <a href="../login/login.php"><img src="user.png" style="height:20px"></a>
+    <a href="../transaksi/index.php"><img src="heart.png" style="height:20px"></a>
+    <a href="#"><img src="shop.png" style="height:20px"></a>
+  </ul>
+</div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="../home/index.php">macflari</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item nav-danger">
+        <a class="nav-link" href="../home/index.php">Home</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Product</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="cleanser.php">c l e a n s e r</a>
+          <a class="dropdown-item" href="mask.php">m a s k</a>
+          <a class="dropdown-item" href="toner.php">t o n e r</a>
+          <a class="dropdown-item" href="serum.php">s e r u m</a>
+          <a class="dropdown-item" href="moist.php">m o i s t</a>
+        </div>
+        </li>
+        <li class="nav-item nav-danger">
+          <a class="nav-link" href="#">Journal</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../contact/us.php">Contact Us</a>
+        </li>
+        <li class="nav-item nav-danger">
+          <a class="nav-link" href="../crud/data.php">Data</a>
+        </li>
+    </ul>
+    <form class="form-inline">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-info" type="submit"><img src="search.png" style="height:20px"></button>
+    </form>
+    <span class="navbar-text"></span>
+  </div>  
+</nav>
+
+<div class="container">
+    <div class="row">
+      <div class="col-lg-3">
+        <h1 class="my-4">Macflari Product</h1>
+        <div class="list-group">
+        <a href="home.php" class="list-group-item">b e s t s e l l e r</a>
+          <a href="cleanser.php" class="list-group-item">c l e a n s e r</a>
+          <a href="mask.php" class="list-group-item">m a s k</a>
+          <a href="toner.php" class="list-group-item">t o n e r</a>
+          <a href="serum.php" class="list-group-item">s e r u m</a>
+          <a href="moist.php" class="list-group-item">m o i s t</a>
+        </div>
+      </div>
+
+      <div class="col-lg-9">
+
+        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+              <img class="d-block img-fluid" src="1.png" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block img-fluid" src="2.png" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block img-fluid" src="3.png" alt="Third slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <img class="card-img-top" src="i1.png" alt="">
+              <div class="card-body">
+                <h4 class="card-title">Hydrating Lip Mask</h4>
+                <h5>IDR 62.000,00</h5>
+                <p class="card-text">Our hydrating lip mask is a moisturizing balm that helps restore dry lips.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9733;ㅤ</small>
+                <a href="#"><img src="shop.png" style="height:20px"></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="i2.png" alt=""></a>
+              <div class="product-action">
+                <div class="card-body">
+                  <h4 class="card-title">Coconut Scrub</h4>
+                  <h5>IDR 413.900</h5>
+                  <p class="card-text">Our luxe sugar scrub is an exclusive recipe of lush nutrients that delivers gentle exfoliation and healthy hydration.</p>
+                </div>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9733;ㅤ</small>
+                <a href="#"><img src="shop.png" style="height:20px"></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="i3.png" alt=""></a>
+              <div class="card-body">
+                <h4 class="card-title">Makeup Removing Wipes</h4>
+                <h5>IDR 63.999</h5>
+                <p class="card-text">Our cleansing towelettes remove dirt, oil and makeup, leaving your skin feeling fresh and looking clean.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9733;ㅤ</small>
+                <a href="#"><img src="shop.png" style="height:20px"></a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+<br><br>
+<footer class="page-footer font-small blue-grey lighten-5">
+<div style="background-color: #EE9A9A;">
+  <div class="container">
+    <div class="row py-4 d-flex align-items-center">
+      <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+        <h6 class="mb-0">Get Connect with Macflari</h6>
+      </div>
+      <div class="col-md-6 col-lg-7 text-center text-md-right">
+        <a href="#"><img src="facebook.png" style="height:20px"></a>
+        <a href="#"><img src="twitter.png" style="height:20px"></a>
+        <a href="#"><img src="instagram.png" style="height:20px"></a>
+        <a href="#"><img src="youtube.png" style="height:20px"></a>
+        <a href="#"><img src="gmail.png" style="height:20px"></a>
+      </div>
+    </div>
+   </div>
+</div>
+<br><br>
+
+<div class="container-fluid text-center text-md-left">
+    <div class="row">
+        <div class="col-md-6 mt-md-0 mt-3"></div>
+        <hr class="clearfix w-100 d-md-none pb-3">
+        <div class="col-md-3 mb-md-0 mb-3">
+        <h5 class="text-uppercase">Company</h5>
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">ABOUT US</a>
+          </li>
+          <li>
+            <a href="#!">OUR EVENT</a>
+          </li>
+          <li>
+            <a href="#!">SITE MAP</a>
+          </li>
+        </ul>
+      </div>
+      
+      <div class="col-md-3 mb-md-0 mb-3">
+        <h5 class="text-uppercase">Help</h5>
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">TERMS & CONDITION</a>
+          </li>
+          <li>
+            <a href="#!">FAQ'S</a>
+          </li>
+          <li>
+            <a href="#!">CONFIRM PAYMENT</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+</div>
+
+<div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="">Macfalri</a>
+  </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+</html>
