f12c64df7281982ee7b79233c41bddc05adf423b
ekaputridr <60589563+ekaputridr@users.noreply.github.com>
2020-04-20 20:24:10 +0700

    Add files via upload

---
+++ about/about.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+<style>
+.social-link {
+  width: 30px;
+  height: 30px;
+  border: 1px solid #ddd;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #666;
+  border-radius: 50%;
+  transition: all 0.3s;
+  font-size: 0.9rem;
+}
+
+.social-link:hover,
+.social-link:focus {
+  background: #ddd;
+  text-decoration: none;
+  color: #555;
+}
+</style>
+<body>
+<br>
+<div class="page-header">
+  <a href="act.php"><center><h10>FREE SHIPPING OVER RP.100,000 FOR MEMBERSHIP</h10></center></a>
+  <ul>
+    <a href="../login/login.php"><img src="../login/user.png" style="height:20px"></a>
+    <a href="../transaksi/index.php"><img src="../login/heart.png" style="height:20px"></a>
+    <a href="#"><img src="../login/shop.png" style="height:20px"></a>
+  </ul>
+</div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="../home/index.php">macflari</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item nav-danger">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Product</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="../product/home.php">b e s t  s e l l e r </a>
+          <a class="dropdown-item" href="../product/cleanser.php">c l e a n s e r</a>
+          <a class="dropdown-item" href="../product/mask.php">m a s k</a>
+          <a class="dropdown-item" href="../product/toner.php">t o n e r</a>
+          <a class="dropdown-item" href="../product/serum.php">s e r u m</a>
+          <a class="dropdown-item" href="../product/moist.php">m o i s t</a>
+        </div>
+        </li>
+        <li class="nav-item nav-danger">
+          <a class="nav-link" href="#">Journal</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../contact/us.php">Contact Us</a>
+        </li>
+    </ul>
+    <form class="form-inline">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-info" type="submit"><img src="../login/search.png" style="height:20px"></button>
+    </form>
+    <span class="navbar-text"></span>
+  </div>  
+</nav>
+
+<div class="bg-red">
+  <div class="container py-5">
+    <div class="row h-100 align-items-center py-5">
+      <div class="col-lg-6">
+        <h1 class="display-4">ABOUT US</h1>
+        <p class="lead text-muted mb-0">Starting from 2019 we want to build a global cosmetics company that is dedicated to the ethical and beauty business.</p>
+        <p class="lead text-muted mb-0">Everything is cruelty free, vegan, gluten free, paraben and sulfate free and suitable for all skin types.</p>
+        <p class="lead text-muted">We hope you get the best from us</p>
+        </div>
+      <div class="col-lg-6 d-none d-lg-block"><img src="1.jpg" alt="" class="img-fluid" style="displau: block;"></div>
+    </div>
+  </div>
+</div>
+
+<div class="bg-light py-5">
+  <div class="container py-5">
+    <div class="row mb-4">
+      <div class="col-lg-5">
+        <h2 class="display-4 font-weight-light">WE</h2>
+        <p class="font-italic text-muted">our associates and production team.</p>
+      </div>
+    </div>
+
+    <div class="row text-center">
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="dasel.jpeg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Dasel Ahn</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="jeno.jpeg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Lee Jeno</h5><span class="small text-uppercase text-muted">Team Management</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="herin.jpeg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Herin Seo</h5><span class="small text-uppercase text-muted">Co-Founder</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="saerom.jpeg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Lee Saerom</h5><span class="small text-uppercase text-muted">Owner</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="olivia.jpeg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Olivia Hye</h5><span class="small text-uppercase text-muted">team product</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<footer class="page-footer font-small blue-grey lighten-5">
+<div style="background-color: #EE9A9A;">
+  <div class="container">
+    <div class="row py-4 d-flex align-items-center">
+      <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+        <h6 class="mb-0">Get Connect with Macflari</h6>
+      </div>
+      <div class="col-md-6 col-lg-7 text-center text-md-right">
+        <a href="#"><img src="../login/facebook.png" style="height:20px"></a>
+        <a href="#"><img src="../login/twitter.png" style="height:20px"></a>
+        <a href="#"><img src="../login/instagram.png" style="height:20px"></a>
+        <a href="#"><img src="../login/youtube.png" style="height:20px"></a>
+        <a href="#"><img src="../login/gmail.png" style="height:20px"></a>
+      </div>
+    </div>
+   </div>
+</div>
+<br><br>
+
+<div class="container-fluid text-center text-md-left">
+    <div class="row">
+        <div class="col-md-6 mt-md-0 mt-3"></div>
+        <hr class="clearfix w-100 d-md-none pb-3">
+        <div class="col-md-3 mb-md-0 mb-3">
+        <h5 class="text-uppercase">Company</h5>
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">ABOUT US</a>
+          </li>
+          <li>
+            <a href="#!">OUR EVENT</a>
+          </li>
+          <li>
+            <a href="#!">SITE MAP</a>
+          </li>
+        </ul>
+      </div>
+      
+      <div class="col-md-3 mb-md-0 mb-3">
+        <h5 class="text-uppercase">Help</h5>
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">TERMS & CONDITION</a>
+          </li>
+          <li>
+            <a href="#!">FAQ'S</a>
+          </li>
+          <li>
+            <a href="#!">CONFIRM PAYMENT</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+</div>
+
+<div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="">Macfalri</a>
+</div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</body>
+</html>
